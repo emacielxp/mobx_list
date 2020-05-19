e292107a44f71a01e9f88d802ac8097377daf442
Ery Maciel <ery.maciel@gmail.com>
2020-05-19 01:04:54 -0300

    App em flutter para demonstrar o uso de MobX com Listas com Observers

---
+++ README.md
@@ -0,0 +1,3 @@
+# MobXList
+
+App em flutter para demonstrar o uso de MobX com Listas com Observers
---
+++ lib/pages/home_controller.dart
@@ -0,0 +1,42 @@
+import 'package:mobx/mobx.dart';
+import 'package:mobxlist/models/item_model.dart';
+part 'home_controller.g.dart';
+
+class HomeController = _HomeControllerBase with _$HomeController;
+
+abstract class _HomeControllerBase with Store {
+  @observable
+  ObservableList<ItemModel> listItems = [
+    ItemModel(title: 'Item 1', check: true),
+    ItemModel(title: 'Item 2', check: false),
+    ItemModel(title: 'Item 3', check: false)
+  ].asObservable();
+
+  @computed
+  int get totalChecked => listItems.where((item) => item.check).length;
+
+  @computed
+  List<ItemModel> get listFiltered {
+    if (filter.isEmpty) {
+      return listItems;
+    } else {
+      return listItems.where((item) => item.title.toLowerCase().contains(filter.toLowerCase())).toList();
+    }
+  }
+
+  @observable
+  String filter = "";
+
+  @action
+  setFilter(String value) => filter = value;
+
+  @action
+  addItem(ItemModel model) {
+    listItems.add(model);
+  }
+
+  @action
+  removeItem(ItemModel model) {
+    listItems.removeWhere((item) => item.title == model.title);
+  }
+}
